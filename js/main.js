7c8e1e1a646c0a4b65b79b0a88b0013994f65fdb
Leonardo Oliveira Balsalobre <57500163+oliveirabalsa@users.noreply.github.com>
2020-06-13 20:04:44 -0300

    Create my portfolio

---
+++ js/main.js
@@ -0,0 +1,50 @@
+// Scroll Suave no Menu
+const menuItens = document.querySelectorAll('#menu-itens a');
+
+menuItens.forEach(item => {
+    item.addEventListener('click', scrollToIdOnClick);
+})
+
+function getScrollTopByHref(element) {
+    const id = element.getAttribute('href');
+    return document.querySelector(id).offsetTop;
+}
+
+function scrollToIdOnClick(event) {
+    event.preventDefault()
+    const to = getScrollTopByHref(event.target)
+    scrollToPosition(to);
+}
+
+function scrollToPosition(to) {
+    window.scroll({
+        top: to,
+        behavior: 'smooth'
+    });
+}
+
+
+
+// Animção da pagina
+
+const target = document.querySelectorAll('[data-anime]');
+const animationClass = 'animate';
+
+function animeScroll() {
+    const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4);
+    target.forEach(function(element) {
+        if(windowTop > element.offsetTop) {
+            element.classList.add(animationClass);
+        } else {
+            element.classList.remove(animationClass);
+        }
+    })
+}
+
+animeScroll();
+
+if(target.length) {
+    window.addEventListener('scroll', function(){
+        animeScroll();
+    })
+}
